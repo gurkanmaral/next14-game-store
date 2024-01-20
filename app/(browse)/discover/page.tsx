2a2e55361c7557c2cdaa65abdd97e06a9fbe5445
gurkanmaral <gurkanmaral1@gmail.com>
2024-01-20 15:11:57 +0300

    fixed discover page fetching error5

--- app/(browse)/discover/page.tsx
+++ app/(browse)/discover/page.tsx
@@ -5,14 +5,19 @@ import PaginationComponent from './_components/Pagination'
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { getFilteredGames } from '@/data/game/get-details';
+import dynamic from 'next/dynamic';
 
-export const dynamic = 'force-dynamic'
 
 const font = Poppins({
     subsets: ["latin"],
     weight: ["100", "300", "400", "700", "900"],
   });
-  
+
+
+  const DiscoverListClient = dynamic(
+    () => import('./_components/discoverList'),
+    { ssr: false }
+  );
 const Discover = async() => {
 
 
@@ -23,7 +28,7 @@ const Discover = async() => {
   return (
     <div className='px-2 pt-10 max-w-screen-2xl mx-0 md:mx-auto gap-10 flex flex-col items-center'>
       <h1 className={cn('text-5xl font-bold',font.className)}>Discover</h1>
-      <DiscoverList />
+      <DiscoverListClient />
       
     </div>
   )
